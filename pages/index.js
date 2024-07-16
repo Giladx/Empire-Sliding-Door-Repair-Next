54c426af59de255ab2171e825ae7f8dc4ecf6739
Gilad Levi <gilad.x@gmail.com>
2024-07-16 15:37:43 +0300

    Commit from TeleportHQ

--- pages/index.js
+++ pages/index.js
@@ -59,9 +59,9 @@ const Home = (props) => {
                   </div>
                 </div>
                 <nav className="home-links">
-                  <span>Home</span>
-                  <span>Services</span>
-                  <span>About Us</span>
+                  <span className="thq-body-small thq-link">Home</span>
+                  <span className="thq-body-small thq-link">Services</span>
+                  <span className="thq-body-small thq-link">About Us</span>
                   <span className="thq-body-small thq-link">Contact</span>
                   <span className="thq-body-small thq-link">Link5</span>
                 </nav>
@@ -184,10 +184,7 @@ const Home = (props) => {
               </div>
             </div>
             <div className="home-row1">
-              <a
-                href="tel:+13054079566"
-                className="thq-body-small thq-link home-link1"
-              >
+              <a href="tel:+13054079566" className="home-link01">
                 <div className="home-content06">
                   <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
                     <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
@@ -211,10 +208,7 @@ const Home = (props) => {
                   </div>
                 </div>
               </a>
-              <a
-                href="tel:+17863482109"
-                className="thq-body-small thq-link home-link2"
-              >
+              <a href="tel:+17863482109" className="home-link02">
                 <div className="home-content08">
                   <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
                     <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
@@ -240,10 +234,7 @@ const Home = (props) => {
                   </div>
                 </div>
               </a>
-              <a
-                href="tel:+19542374136"
-                className="thq-body-small thq-link home-link3"
-              >
+              <a href="tel:+19542374136" className="home-link03">
                 <div className="home-content10">
                   <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
                     <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
@@ -280,6 +271,114 @@ const Home = (props) => {
                     </span>
                   </div>
                 </div>
+              </a>
+            </div>
+            <div className="home-row2">
+              <a href="tel:+17862064210" className="home-link04">
+                <div className="home-content12">
+                  <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                    <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                  </svg>
+                  <div className="home-contact-info3">
+                    <div className="home-content13">
+                      <h3 className="home-text34 thq-heading-3">
+                        Empire sliding door repair - Lake Worth ,Florida
+                      </h3>
+                      <p className="home-text35 thq-body-large">
+                        <span>5805 Eddy Ct</span>
+                        <br></br>
+                        <span>Lake Worth, FL 33463</span>
+                        <br className="home-text39"></br>
+                        <span>United States</span>
+                      </p>
+                    </div>
+                    <span className="home-address3 thq-body-small">
+                      +17862064210
+                    </span>
+                  </div>
+                </div>
+              </a>
+              <a href="tel:+17862040489" className="home-link05">
+                <div className="home-content14">
+                  <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                    <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                  </svg>
+                  <div className="home-contact-info4">
+                    <div className="home-content15">
+                      <h3 className="home-text41 thq-heading-3">
+                        <span>Empire sliding door repair -</span>
+                        <br></br>
+                        <span> Boca Raton ,Florida</span>
+                      </h3>
+                      <p className="home-text45 thq-body-large">
+                        <span>647 NW 12th Rd</span>
+                        <br className="home-text47"></br>
+                        <span>Boca Raton, FL 33486</span>
+                        <br className="home-text49"></br>
+                        <span>United States</span>
+                      </p>
+                    </div>
+                    <span className="home-address4 thq-body-small">
+                      +17862040489
+                    </span>
+                  </div>
+                </div>
+              </a>
+              <a href="tel:+17863147054" className="home-link06">
+                <div className="home-content16">
+                  <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                    <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                  </svg>
+                  <div className="home-contact-info5">
+                    <div className="home-content17">
+                      <h3 className="home-text51 thq-heading-3">
+                        Empire sliding door repair - Doral ,Florida
+                      </h3>
+                      <p className="home-text52 thq-body-large">
+                        <span>5861 NW 112th Ct</span>
+                        <br></br>
+                        <span>Doral, FL 33178</span>
+                        <br className="home-text56"></br>
+                        <span>United States</span>
+                      </p>
+                    </div>
+                    <span className="home-address5 thq-body-small">
+                      +17863147054
+                    </span>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="home-row3">
+              <a href="tel:+17862065938" className="home-link07">
+                <div className="home-content18">
+                  <svg viewBox="0 0 1024 1024" className="thq-icon-medium">
+                    <path d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"></path>
+                  </svg>
+                  <div className="home-contact-info6">
+                    <div className="home-content19">
+                      <h3 className="home-text58 thq-heading-3">
+                        Empire sliding door repair - Naples ,Florida
+                      </h3>
+                      <p className="home-text59 thq-body-large">
+                        <span>9066 Shenendoah Cir</span>
+                        <br className="home-text61"></br>
+                        <span>Naples, FL 34113</span>
+                        <br className="home-text63"></br>
+                        <span>United States</span>
+                      </p>
+                    </div>
+                    <span className="home-address6 thq-body-small">
+                      +17862065938
+                    </span>
+                  </div>
+                </div>
+              </a>
+              <a href="tel:+17862040489" className="home-link08">
+                <div className="home-content20"></div>
+              </a>
+              <a href="tel:+17863147054" className="home-link09">
+                <div className="home-content21"></div>
               </a>
             </div>
           </div>
@@ -553,7 +652,7 @@ const Home = (props) => {
             align-self: stretch;
             align-items: flex-start;
           }
-          .home-link1 {
+          .home-link01 {
             display: contents;
           }
           .home-content06 {
@@ -601,7 +700,7 @@ const Home = (props) => {
             text-align: center;
             font-family: 'Exo 2';
           }
-          .home-link2 {
+          .home-link02 {
             display: contents;
           }
           .home-content08 {
@@ -649,7 +748,7 @@ const Home = (props) => {
             text-align: center;
             font-family: 'Exo 2';
           }
-          .home-link3 {
+          .home-link03 {
             display: contents;
           }
           .home-content10 {
@@ -696,6 +795,272 @@ const Home = (props) => {
           .home-address2 {
             text-align: center;
             font-family: 'Exo 2';
+          }
+          .home-row2 {
+            gap: var(--dl-space-space-threeunits);
+            display: flex;
+            align-self: stretch;
+            align-items: flex-start;
+          }
+          .home-link04 {
+            display: contents;
+          }
+          .home-content12 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 1px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content12:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
+          }
+          .home-contact-info3 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            display: flex;
+            align-self: stretch;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-content13 {
+            gap: var(--dl-space-space-unit);
+            display: flex;
+            align-self: stretch;
+            align-items: stretch;
+            flex-direction: column;
+          }
+          .home-text34 {
+            align-self: stretch;
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text35 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text39 {
+            font-family: 'Exo 2';
+          }
+          .home-address3 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-link05 {
+            display: contents;
+          }
+          .home-content14 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 1px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content14:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
+          }
+          .home-contact-info4 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            display: flex;
+            align-self: stretch;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-content15 {
+            gap: var(--dl-space-space-unit);
+            display: flex;
+            align-self: stretch;
+            align-items: stretch;
+            flex-direction: column;
+          }
+          .home-text41 {
+            align-self: stretch;
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text45 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text47 {
+            font-family: 'Exo 2';
+          }
+          .home-text49 {
+            font-family: 'Exo 2';
+          }
+          .home-address4 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-link06 {
+            display: contents;
+          }
+          .home-content16 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 1px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content16:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
+          }
+          .home-contact-info5 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            display: flex;
+            align-self: stretch;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-content17 {
+            gap: var(--dl-space-space-unit);
+            display: flex;
+            align-self: stretch;
+            align-items: stretch;
+            flex-direction: column;
+          }
+          .home-text51 {
+            align-self: stretch;
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text52 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text56 {
+            font-family: 'Exo 2';
+          }
+          .home-address5 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-row3 {
+            gap: var(--dl-space-space-threeunits);
+            display: flex;
+            align-self: stretch;
+            align-items: flex-start;
+          }
+          .home-link07 {
+            display: contents;
+          }
+          .home-content18 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 1px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content18:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
+          }
+          .home-contact-info6 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            display: flex;
+            align-self: stretch;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-content19 {
+            gap: var(--dl-space-space-unit);
+            display: flex;
+            align-self: stretch;
+            align-items: stretch;
+            flex-direction: column;
+          }
+          .home-text58 {
+            align-self: stretch;
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text59 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-text61 {
+            font-family: 'Exo 2';
+          }
+          .home-text63 {
+            font-family: 'Exo 2';
+          }
+          .home-address6 {
+            text-align: center;
+            font-family: 'Exo 2';
+          }
+          .home-link08 {
+            display: contents;
+          }
+          .home-content20 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            border: 2px dashed rgba(120, 120, 120, 0.4);
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 0px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content20:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
+          }
+          .home-link09 {
+            display: contents;
+          }
+          .home-content21 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            flex: 1;
+            width: 100%;
+            border: 2px dashed rgba(120, 120, 120, 0.4);
+            cursor: pointer;
+            display: flex;
+            padding: var(--dl-space-space-twounits);
+            transition: 0.3s;
+            align-items: center;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 0px;
+            border-radius: var(--dl-radius-radius-imageradius);
+            flex-direction: column;
+            text-decoration: none;
+          }
+          .home-content21:hover {
+            box-shadow: 5px 5px 10px 0px #afafaf;
           }
           @media (max-width: 1600px) {
             .home-image1 {
@@ -789,6 +1154,12 @@ const Home = (props) => {
             .home-row1 {
               flex-direction: column;
             }
+            .home-row2 {
+              flex-direction: column;
+            }
+            .home-row3 {
+              flex-direction: column;
+            }
           }
           @media (max-width: 479px) {
             .home-navbar-interactive {
@@ -829,6 +1200,12 @@ const Home = (props) => {
               width: 100%;
             }
             .home-row1 {
+              align-items: stretch;
+            }
+            .home-row2 {
+              align-items: stretch;
+            }
+            .home-row3 {
               align-items: stretch;
             }
           }
